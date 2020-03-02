b43310b111d63b0597b6ab6af694debdc56a245b
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-02 05:50:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -125,7 +125,7 @@ def shoe_size(name)
 end
 
 def team_colors(team_name)
-  game_hash.each do |place,team|
+  game_hash.each do |place, team|
     if team [:team_name] == team_name
       return team[:colors]
 end
