9d4635207bc82bb35a917ab49b9dbaa03754e8e5
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-02 04:05:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,7 +119,12 @@ def game_hash
 end
 
 def num_points_scored (players_name)
-  game_hash.each do| 
+  game_hash.each do|place,team| 
+  team.each do |attribute,data| 
+    if attribute == :players 
+      data.each do |player| 
+        if player[:player_name] === player_name
+          return player[:points]
   
   
 end
