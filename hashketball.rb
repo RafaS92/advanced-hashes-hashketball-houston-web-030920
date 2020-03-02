d4b1f1bcf8cb382feedef755c3848cadb7343800
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-02 05:55:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,7 +127,7 @@ end
 def team_colors(team_name)
   game_hash.each do |place, team|
     if team [:team_name] == team_name
-      return team[:colors].join("")
+      return team[:colors]
 end
 end 
 end
