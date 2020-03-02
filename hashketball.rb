0fd29ecaa4412c5c2fdb2244c9fe408bf35a7d1e
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-02 04:40:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,8 @@ def game_hash
       :steals => 3,
       :blocks => 1,
       :slam_dunks => 1
-   }, {
+   },
+   {
       :player_name => "Reggie Evans",
       :number => 30,
       :shoe => 14,
@@ -24,7 +25,8 @@ def game_hash
       :steals => 12,
       :blocks => 12,
       :slam_dunks => 7
-   }, {
+   }, 
+   {
       :player_name => "Brook Lopez",
       :number => 11,
       :shoe => 17,
@@ -34,7 +36,8 @@ def game_hash
       :steals => 3,
       :blocks => 1,
       :slam_dunks => 15
-   }, {
+   }, 
+   {
      :player_name => "Mason Plumlee",
       :number => 1,
       :shoe => 19,
@@ -44,7 +47,8 @@ def game_hash
       :steals => 3,
       :blocks => 8,
       :slam_dunks => 5
-   }, {
+   }, 
+   {
      :player_name => "Jason Terry",
       :number => 31,
       :shoe => 15,
@@ -92,7 +96,8 @@ def game_hash
       :steals => 4,
       :blocks => 5,
       :slam_dunks => 5
-     }, {
+     },
+     {
        :player_name => "Ben Gordon",
       :number => 8,
       :shoe => 15,
@@ -102,7 +107,8 @@ def game_hash
       :steals => 1,
       :blocks => 1,
       :slam_dunks => 0
-     }, {
+     },
+     {
        :player_name => "Brendan Haywood",
       :number => 33,
       :shoe => 15,
