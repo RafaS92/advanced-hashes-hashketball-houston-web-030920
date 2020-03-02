3ddf7898a2e2a39cbc8e1e1b748f87bc374a72f1
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-02 05:32:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,6 +16,7 @@ def game_hash
         :blocks => 1,
         :slam_dunks => 1
       },
+      {
       :player_name =>"Reggie Evans",
         :number => 30,
         :shoe => 14,
@@ -26,6 +27,7 @@ def game_hash
         :blocks => 12,
         :slam_dunks => 7
       },
+      {
       :player_name =>"Brooke Lopez",
         :number => 11,
         :shoe => 17,
@@ -36,6 +38,7 @@ def game_hash
         :blocks => 1,
         :slam_dunks => 15
       },
+      {
       :player_name =>"Mason Plumlee",
         :number => 1,
         :shoe => 19,
@@ -46,6 +49,7 @@ def game_hash
         :blocks => 8,
         :slam_dunks => 5
       },
+      {
       :player_name =>"Jason Terry",
         :number => 31,
         :shoe => 15,
@@ -71,6 +75,7 @@ def game_hash
         :blocks => 7,
         :slam_dunks => 2
       },
+      {
       :player_name =>"Bismak Biyombo",
         :number => 0,
         :shoe => 16,
@@ -81,6 +86,7 @@ def game_hash
         :blocks => 15,
         :slam_dunks => 10
       },
+      {
       :player_name =>"DeSagna Diop",
         :number => 2,
         :shoe => 14,
@@ -91,6 +97,7 @@ def game_hash
         :blocks => 5,
         :slam_dunks => 5
       },
+      {
       :player_name =>"Ben Gordon",
         :number => 8,
         :shoe => 15,
@@ -101,6 +108,7 @@ def game_hash
         :blocks => 1,
         :slam_dunks => 0
       },
+      {
       :player_name =>"Brendan Haywood",
         :number => 33,
         :shoe => 15,
